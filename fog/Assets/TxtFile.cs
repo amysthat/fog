f15a55a80f45c7bdfd2c50ef196dc2b7ae8547c8
amysthat <arduinomacerasi@gmail.com>
2024-02-24 16:05:07 +0300

    Made TxtFile's an Object.

--- fog/Assets/TxtFile.cs
+++ fog/Assets/TxtFile.cs
@@ -1,20 +1,21 @@
-﻿using System.Text;
+﻿using fog.Memory;
+using System.Text;
 
 namespace fog.Assets
 {
     /// <summary>
     /// Standard text file, imported with UTF-8
     /// </summary>
-    public class TxtFile
+    public class TxtFile : Object
     {
         public string text { get; set; }
 
         public static TxtFile FromBytes(byte[] content)
         {
-            return new TxtFile
-            {
-                text = Encoding.UTF8.GetString(content),
-            };
+            var txtFile = MemoryManager.Allocate<TxtFile>();
+            txtFile.text = Encoding.UTF8.GetString(content);
+
+            return txtFile;
         }
     }
 }
